c93fba8bc2f6f55d2f1a511c374100b21c610737
Nicolas Welti <nicolas.welti@free.fr>
2021-01-23 17:53:34 +0100

    temps

--- PythonTIPE/Fenetre.py
+++ PythonTIPE/Fenetre.py
@@ -5,56 +5,63 @@ import time
 
 class Fenetre(tk.Tk):
     """La fenêtre qui affichera la forêt simulée."""
-
     def __init__(self):
         super().__init__()
         #la forêt qui sera simulée.
-        self.foret=Foret(75,150)
+        self.foret = Foret(75,150)
         #Grille d'affichage correspondant à la grille forêt.
-        self.rectGrid=[[(None,None) for i in range(self.foret.nC)] for j in range(self.foret.nL)]
+        self.rectGrid = [[(None,None) for i in range(self.foret.nC)] for j in range(self.foret.nL)]
         #taille d'affichage d'un carreau de la grille forêt.
         self.a = 10
+        #durée d'un tick (250 ms réelles) dans la simulation (en minutes).
+        self.tickDuration = 5
+        #temps écoulé dans la simulation (minutes) elle est en chiffre contrairement à timeElapsed qui est le texte affiché.
+        self.tElapsed = 0
         #Code couleur pour faire correspondre la simulation à l'affichage.
-        self.colorCode={0:'forest green',1:'red',2:'ivory',3:'gray29'}
+        self.colorCode = {0:'forest green',1:'red',2:'ivory',3:'gray29'}
         #initialisation
         self.creerWidgets()
-        self.playing=False
-        self.skip=False
+        self.playing = False
+        self.skip = False
         self.suivant()
     
     def creerWidgets(self):
         """Création et disposition des widgets."""
         self.title("Le Feu")
         #le canvas pour afficher la grille sur l'écran.
-        self.canvas = tk.Canvas(self, width=15*self.a*self.foret.nC+1, height=self.a*self.foret.nL+1, highlightthickness=2)
+        self.canvas = tk.Canvas(self, width=15 * self.a * self.foret.nC + 1, height=self.a * self.foret.nL + 1, highlightthickness=2)
         self.creerGrille()
 
         #le bouton pour allumer le feu
-        fra=tk.Frame(self)
-        self.bouText=tk.StringVar()
-        #le texte du bouton est dynamique, il change selon si l'on est au début, en pause ou en cours de propagation.
+        fra = tk.Frame(self)
+        self.bouText = tk.StringVar()
+        #le texte du bouton est dynamique, il change selon si l'on est au
+        #début, en pause ou en cours de propagation.
         self.bouText.set("Commencer")
         self.bou1 = tk.Button(fra,textvariable=self.bouText, width=15, command=self.play)
         self.bou1.pack(side='left')
         #le slider qui permet de modifier l'angle du vent
-        la=tk.Label(fra,text="Angle du vent (°)")
+        la = tk.Label(fra,text="Angle du vent (°)")
         la.pack(side='right')
         self.scala2 = tk.Scale(fra, from_=0, to=360, orient=tk.HORIZONTAL)
         self.scala2.set(45)
         self.scala2.pack(side='right')
         #slider pour l'humidité
-        la=tk.Label(fra,text="humidité (%)")
+        la = tk.Label(fra,text="humidité (%)")
         la.pack(side='right')
-        self.scala_humid = tk.Scale(fra, from_=1, to=50, orient=tk.HORIZONTAL)
-        self.scala_humid.set(20)
+        self.scala_humid = tk.Scale(fra, from_=5, to=50, orient=tk.HORIZONTAL)
+        self.scala_humid.set(int(self.foret.humidite_defaut * 100))
         self.scala_humid.pack(side='right')
         #slider pour la vitesse du vent
-        la=tk.Label(fra,text="vitesse du vent (m/s)")
+        la = tk.Label(fra,text="vitesse du vent (m/s)")
         la.pack(side='right')
-        self.scala_vitesse = tk.Scale(fra, from_=0, to=10, orient=tk.HORIZONTAL)
-        self.scala_vitesse.set(2)
+        self.scala_vitesse = tk.Scale(fra, from_=0, to=40, orient=tk.HORIZONTAL)
+        self.scala_vitesse.set(self.foret.v_vent_defaut)
         self.scala_vitesse.pack(side='right')
-
+        self.timeElapsed = tk.StringVar()
+        self.timeElapsed.set("temps écoulé: 0h 00 min")
+        self.timeLab = tk.Label(fra,textvariable=self.timeElapsed)
+        self.timeLab.pack(side='right')
         fra.pack()
         self.canvas.pack(fill='both')
 
@@ -62,25 +69,25 @@ class Fenetre(tk.Tk):
         """Affichage sur la fenêtre des modifications enregistrées dans la forêt."""
         for x in range(self.foret.nL):
             for y in range(self.foret.nC):
-                coul=self.colorCode[self.foret.grille[x,y]]
-                recta,coulor=self.rectGrid[x][y]
+                coul = self.colorCode[self.foret.grille[x,y]]
+                recta,coulor = self.rectGrid[x][y]
                 #la modification de la grille d'affichage n'a lieu que quand nécessaire afin d'améliorer les performances.
                 if coul != coulor:
                     #modification de la couleur du rectangle affiché sur l'écran.
                     self.canvas.itemconfig(recta, fill=coul)
                     #modification du rectangle et de la couleur stockés dans la grille d'affichage.
-                    self.rectGrid[x][y]=(recta,coul)
-        #l'affichage est directement mis à jour (les couleurs changent sur l'écran)
+                    self.rectGrid[x][y] = (recta,coul)
+        #l'affichage est directement mis à jour (les couleurs changent sur l'écran).
         self.canvas.update()
 
     def creerGrille(self):
         """Création et initialisation des carreaux qui constituent la grille de la fenêtre."""
         for x in range(self.foret.nL):
             for y in range(self.foret.nC):
-                coul=self.colorCode[self.foret.grille[x,y]]
-                recta=self.canvas.create_rectangle((x*self.a, y*self.a, (x+1)*self.a, (y+1)*self.a), outline="gray", fill=coul)
+                coul = self.colorCode[self.foret.grille[x,y]]
+                recta = self.canvas.create_rectangle((x * self.a, y * self.a, (x + 1) * self.a, (y + 1) * self.a), outline="gray", fill=coul)
                 #les rectangles sont affichés puis stockés avec leur couleur dans la grille d'affichage.
-                self.rectGrid[x][y]=(recta,coul)
+                self.rectGrid[x][y] = (recta,coul)
 
     def play(self):
         self.playing = (not self.playing)
@@ -98,22 +105,30 @@ class Fenetre(tk.Tk):
             self.bouText.set("Reprendre")
         if self.playing:
             #après avoir repris on enregistre les éventuelles modifications de l'angle du vent.
-            self.foret.mesher_vitesse(self.scala_vitesse.get(),np.pi/180 * self.scala2.get(),self.scala_humid.get()/100)
+            self.foret.calculer_modele(self.scala_vitesse.get(),np.pi / 180 * self.scala2.get(),self.scala_humid.get() / 100)
 
     def suivant(self):
         """Fonction appelée régulièrement pour faire avancer la simulation."""
         #itération suivante appelée au début pour être sûr que l'intervalle soit constant.
-        self.after(200,self.suivant)
+        self.after(250,self.suivant)
         #la forêt n'est pas modifiée si on est en pause.
         if self.playing:
             #évolution de la forêt.
             self.foret.evolutionFeu()
+            #avancée du temps.
+            self.tElapsed += int(self.tickDuration * self.foret.distortion_temps)
+            hours = self.tElapsed // 60
+            minutes = self.tElapsed % 60
+            minutes = str(minutes)
+            if len(minutes) == 1:
+                minutes = '0' + minutes
+            self.timeElapsed.set("temps écoulé: " + str(hours) + "h " + minutes + " min")
             #affichage des modifications de la forêt sur la grille.
             if not self.skip:
-                #le skip est utilisé dans le cas où la modification de l'affichage prend plus de 200ms.
+                #le skip est utilisé dans le cas où la modification de l'affichage prend plus de 250ms.
                 #éviter que l'itération suivante modifie l'affichage tant que cette itération n'a pas fini.
                 #les problèmes de performance ne ralentiront donc pas le feu.
                 #en revanche, en cas de problème de performance, l'affichage sera mis à jour à des intervalles plus espacés.
-                self.skip=True
+                self.skip = True
                 self.modifierGrille()
-                self.skip=False
+                self.skip = False
--- PythonTIPE/Foret.py
+++ PythonTIPE/Foret.py
@@ -10,45 +10,50 @@ class Foret(object):
     """La simulation de la forêt qui sera affichée."""
 
     def __init__(self,nC,nL):
-        #taille de la forêt
+        #taille de la forêt.
         self.nC = nC
         self.nL = nL
+        #paramètre (coeff de l'humidité utilisé à deux endroits).
+        self.a=2.10
         #probabilités de base.
         self.pNonArbre = 0.25
-        #TODO: la probabilité de consommation devrait être revue pour avoir un lien avec la vitesse sans vent.
-        #c'est le problème des échelles arbitraire de temps et de distance.
-        #un feu très lent doit s'arrêter plus vite.
-        self.pConsume = 0.02
+        #coefficient d'extinction
+        self.coeffExtinction = 0.05
+        #probabilité d'extinction des cases à l'intérieur du périmètre du feu (pas de végétal adjacent).
+        self.coeffExtinction_interieur=0.2
+        #valeurs par défaut des paramètres.
+        self.distortion_temps=1
+        self.v_vent_defaut=3
+        self.humidite_defaut=0.2
         #initialisation
         self.grille = self.initialiserForet()
-        self.mesher_vitesse(2,np.pi / 4, 0.2)
+        self.calculer_modele(self.v_vent_defaut,np.pi / 4, self.humidite_defaut)
         
     def initialiserForet(self):
         """Initialisation de la forêt."""
-        # creation de la grille
+        # creation de la grille.
         etat = np.zeros((self.nL,self.nC))
-        #initialisation de la grille
+        #initialisation de la grille.
         for i in range(self.nL):
             for j in range(self.nC):
-                #Certaines zones ne seront pas des arbres, à revoir quand la densité sera introduite.
                 if (random.random() < self.pNonArbre) or (i == (self.nL - 1)) or (i == 0) or j == (self.nC - 1) or (j == 0):
                     etat[i,j] = 2
-        # choix du départ du feu
+        # choix du départ du feu.
         etat[self.nL // 2,self.nC // 2] = 1
         return etat
 
     def evolutionFeu(self):
-        """Evolution du feu"""
-        #la grille est copiée afin que les modifications faites durant cette
-        #itération n'affecte pas les calculs.
+        """Evolution du feu: calcul des probabilités et tirage."""
+
         #tous ces calculs sont fait via numpy sans boucle for.
         #numpy lance les calculs directement en C ce qui permet
 
-        #on copie la grille pour s'assurer que les calculs soient effectués avant les modifications
+        #voisinages
+        #on copie la grille pour s'assurer que les calculs soient effectués avant les modifications.
         grille1 = self.grille.copy()
         #les strides sont habituellement utilisées pour du traitement d'image
         #par exemple pour flouter une image en remplaçant un point par la moyenne de ses voisins.
-        #Le principe est d'augmenter le nombre de dimensions ici en passant de deux à 4 dimensions
+        #Le principe est d'augmenter le nombre de dimensions ici en passant de deux à 4 dimensions.
         #on transforme un tableau de nombres en un tableau de matrices 3x3
         #la matrice 3x3 qui remplacent le nombre contient le nombre lui-même et ses 8 voisins.
         #les strides manipulent directement la représentation binaire du tableau dans la mémoire.
@@ -56,6 +61,7 @@ class Foret(object):
         #plus d'info ici: https://realpython.com/numpy-array-programming/#image-feature-extraction
         shape = (grille1.shape[0] - 2, grille1.shape[1] - 2, 3, 3)
         patches = stride_tricks.as_strided(grille1, shape=shape, strides=(2*grille1.strides))
+
         #allumage
         #on coupe les bords car les cases sans voisins n'ont pas été stridées.
         #la multiplication termes à termes annulera les coefficients de vitesses pour les cases éteintes.
@@ -67,9 +73,9 @@ class Foret(object):
         grille2=self.grille[1:(self.nL-1),1:(self.nC-1)]
         #on compare ces nombres avec les probabilités d'allumage et on allume si le nombre est en-dessous de la probabilité.
         grille2[(rdms<allumage) & (grille2 == 0)] = 1
+
         #extinction
-        #toutes les cases en feu ont un probabilité de base de s'éteindre (très faible).
-        #mais les cases n'ayant pas de végétation adjacentes sont considérées comme ayant plus de chances de s'éteindre.
+        #cases n'ayant pas de végétation adjacentes sont considérées comme ayant plus de chances de s'éteindre.
         #le front de flamme étant déjà passé aux autres cases, le peu de feu qui reste s'éteint plus facilement.
         #On considère ces cases comme "finies".
         extinction = (patches==0)#cases de végétal
@@ -77,38 +83,35 @@ class Foret(object):
         extinction=extinction.sum(axis=(-1, -2))
         #le tableau contient maintenant le nombre de cases de végétal adjacentes pour chaque case.
         rdms=np.random.rand(self.nL-2,self.nC-2)#tirage
-        grille2[(extinction == 0) & (grille2 == 1) & (rdms < 0.1)] = 3
+        pcs_interieur=1-np.exp(-self.coeffExtinction_interieur*self.distortion_temps)
+        grille2[(extinction == 0) & (grille2 == 1) & (rdms < pcs_interieur)] = 3
+        #la probabilité d'extinction étant adjacente à des cases de végétal.
+        pcons_perimetre=1-np.exp(-self.coeffExtinction*self.distortion_temps)
+        grille2[(extinction != 0) & (rdms < pcons_perimetre) & (grille2 == 1)] = 3
+
         #update
         self.grille[1:(self.nL-1),1:(self.nC-1)]=grille2.copy()
 
-        #consumé
-        #opération séparée car non basée sur les cases adjacentes et ne nécessitant donc pas de couper les bords.
-        #toutes les cases en feu ont un probabilité de base de s'éteindre (très faible).
-        rdms=np.random.rand(self.nL,self.nC)#tirage
-        self.grille[(rdms < self.pConsume) & (self.grille == 1)] = 3
-
-
 ### modèle physique
 
-    def mesher_vitesse(self,un,alpha,humidite):
-        mat_direction=self.calculerEffetDirectionVent(alpha)
-        #à chaque point on associe sa matrice de vitesse 3x3.
-        #temps de calcul peut-être long surtout une fois la densité de végétation prise en compte.
-        #Mais il n'est effectué que quand que la vitesse du vent est changée.
-        #voir pour plus d'opti quand la densité de végétation sera prise en compte.
-        #par exemple calculer la matrice de direction du vent au début ce qui permettrait de ne recalculer que r pour chaque case.
-        #sinon probablement utiliser numpy ici aussi pour améliorer les performances.
-        base = np.ones((3,3))
+    def calculer_modele(self,un,alpha,humidite):
+        """Calcul des tableaux de vitesses et de la distortion temporelle en fonction des nouvelles valeurs des paramètres."""
+        #calcul de la matrice de vitesse
+        mat_direction=self.calculerMatriceDirectionVent(alpha)
+        #à chaque point on associe sa matrice de vitesse 3x3
+        vit=np.maximum(self.r(un,humidite)*mat_direction,np.ones((3,3)))
         arr=np.ones((self.nL,self.nC,3,3))
         for i in range(self.nL):
             for j in range(self.nC):
                 #pour l'instant, pas de densité de végétation donc la vitesse est pareille partout.
-                arr[i,j]=self.r(un,humidite,1)*mat_direction.copy()+base
+                arr[i,j]=vit
         self.mesh_v=arr
+        #selon les rapports de vitesse, le temps est accéléré ou décéléré
+        self.distortion_temps=((1+self.a*humidite)/(1+self.a*self.humidite_defaut))*(self.r(self.v_vent_defaut,self.humidite_defaut)/(self.r(un,humidite)))
 
 
-    def calculerEffetDirectionVent(self,alpha):
-        """Calcule l'effet du vent."""
+    def calculerMatriceDirectionVent(self,alpha):
+        """Calcule la matrice de direction du vent."""
         vitesse_vent = np.zeros((3,3))
         for i in range(8):
             #on fait tourner un angle par les 8 directions.
@@ -135,13 +138,12 @@ class Foret(object):
             return 0
         return int(x / abs(x))
 
-    def r(self,un,humidite,d):
+    def r(self,un,humidite):
         """Fonction clé du modèle physique, renvoie le rapport entre la vitesse sans vent, et la vitesse avec le vent donné."""
         A0=5.73
         b0=0.23
-        a=2.10
-        rac=1+((b0*un-d)/((b0*un)**2 + d**2)**(1/2))
-        A=(A0/((1+a*humidite)**3))*rac
+        rac=1+((b0*un-1)/((b0*un)**2 +1)**(1/2))
+        A=(A0/((1+self.a*humidite)**3))*rac
         A13=A**(1/3)
         B=A**2+9*A+3*((A+81/4)**(1/2)) + 27/2
         num=B**(2/3) + A13*(A+6)
@@ -149,14 +151,3 @@ class Foret(object):
         r=1+(A13/3)*((num/den)+A13**2)
         return r
 
-humidite=np.linspace(0.05,0.35,100)
-un=np.linspace(1,10,100)
-humidite,un=np.meshgrid(humidite,un)
-r=Foret(10,10).r(un,humidite,0.1)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(humidite, un, r,cmap=cm.plasma)
-ax.set_zlabel('r = cii/ci')
-ax.set_xlabel('humidité')
-ax.set_ylabel('vitesse du vent')
-plt.show()
